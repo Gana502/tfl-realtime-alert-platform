27baecf22dbaf3a0f5f55b95679d816817669344
Gana502 <42062494+Gana502@users.noreply.github.com>
2026-06-25 16:21:44 +0100

    Logger added

---
+++ common/logger.py
@@ -0,0 +1,20 @@
+import logging
+
+
+def get_logger(name):
+
+    logger = logging.getLogger(name)
+
+    logger.setLevel(logging.INFO)
+
+    console_handler = logging.StreamHandler()
+
+    formatter = logging.Formatter(
+        "%(asctime)s | %(levelname)s | %(message)s"
+    )
+
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+
+    return logger
