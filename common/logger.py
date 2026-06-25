b4276e199762f1685b106a11487a389673f11c0d
Gana502 <42062494+Gana502@users.noreply.github.com>
2026-06-25 17:52:17 +0100

    Simplified logger code

--- common/logger.py
+++ common/logger.py
@@ -2,19 +2,19 @@ import logging
 
 
 def get_logger(name):
-
+    # Create a logger with the specified name
     logger = logging.getLogger(name)
-
+    # Set the logging level to INFO
     logger.setLevel(logging.INFO)
-
+    # Create a console handler to output logs to the console
     console_handler = logging.StreamHandler()
-
+    # Set the logging level for the console handler to INFO
     formatter = logging.Formatter(
         "%(asctime)s | %(levelname)s | %(message)s"
     )
-
+    # Set the formatter for the console handler
     console_handler.setFormatter(formatter)
-
+    # Add the console handler to the logger
     logger.addHandler(console_handler)
-
+    # Return the configured logger
     return logger
